08938bd2ae1d0b9ee257f613a0969c7fdfb92d50
GRACE <kwaham.grace@gmail.com>
2024-10-16 08:35:02 +0100

    modifications done

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -169,13 +169,13 @@ CASDOOR_USERINFO_ENDPOINT = os.getenv('CASDOOR_USERINFO_ENDPOINT')
 
 
 CASDOOR_CONFIG = {
-    'endpoint': 'http://172.18.0.4:8000/',
+    'endpoint': 'http://172.18.0.3:8000/',
     'client_id': '215f192e56edb2796f77' , 
     'client_secret': 'adf476c456b764f157b6ebb4e4d6436ba430b07c',
     'redirect_uri': 'http://127.0.0.1:8001/callback/',
-    'authorization_endpoint': 'http://172.18.0.4:8000/login/oauth/authorize',
-    'token_endpoint': 'http://172.18.0.4:8000/api/login/oauth/access_token',
-    'userinfo_endpoint': 'http://172.18.0.4:8000/api/userinfo'
+    'authorization_endpoint': 'http://172.18.0.3:8000/login/oauth/authorize',
+    'token_endpoint': 'http://172.18.0.3:8000/api/login/oauth/access_token',
+    'userinfo_endpoint': 'http://172.18.0.3:8000/api/userinfo'
 }
 
 
